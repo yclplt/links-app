b2e8c9f397e904485b29332de37a3834ba32e76c
Yücel POLAT <39365807+yclplt@users.noreply.github.com>
2022-03-30 10:27:36 +0300

    Update index.js

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,11 +1,9 @@
-import { Image, Label, Divider } from "../../index";
+import { Label, Divider } from "../../index";
 import "./styles.css";
-import logo from "../../../assets/logo/logo.png";
 const Header = ({ label }) => {
   return (
     <>
       <div className="top-header">
-        <Image src={logo} width={150} preview={false} />
         <Label label={label} />
       </div>
       <Divider />
